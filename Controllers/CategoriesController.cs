ca617cad324c15fb2fc590ec391a30c2bf603c51
gc200295036 <nickbrazda95@hotmail.com>
2020-09-27 15:55:37 -0400

    Add project files.

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //- Each category links to /categories/details and passes the category name as a link parameter In your controller, add a function called Details that accepts 1 string parameter.This method sends back a string to the view using the ViewBag that says "You selected category: [paramater-value-here]"
+        public IActionResult Details(string category)
+        {
+            ViewBag.category = category;
+            return View();
+        } 
+    }
+}
